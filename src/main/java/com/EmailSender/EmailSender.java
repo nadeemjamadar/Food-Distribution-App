0b434d95dd3e5e086b51aea2ade6b13f0766f67c
MuzammalHussain6313 <51987198+MuzammalHussain6313@users.noreply.github.com>
2020-04-06 22:03:57 +0500

    Create EmailSender.java

---
+++ src/main/java/com/EmailSender/EmailSender.java
@@ -0,0 +1,26 @@
+package com.EmailSender;
+
+import com.EmailSender.dto.MailRequest;
+import com.EmailSender.dto.MailResponse;
+import com.EmailSender.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmailSender
+{
+    @Autowired
+    private EmailService service;
+
+    @PostMapping("/sendingEmail")
+    public MailResponse sendEmail(@RequestBody MailRequest request) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("Name", request.getName());
+        model.put("location", "Bangalore,India");
+        return service.sendEmail(request, model);
+
+    }
+}
