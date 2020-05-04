a4d848b4172090f1597fc1595dd9b69e19a373dd
MuzammalHussain6313 <51987198+MuzammalHussain6313@users.noreply.github.com>
2020-05-04 10:20:10 +0500

    template was added to call end point of same application at @Controller and getTamplete endpoint.

--- src/main/java/com/EmailSender/service/EmailService.java
+++ src/main/java/com/EmailSender/service/EmailService.java
@@ -3,18 +3,14 @@ package com.EmailSender.service;
 import com.EmailSender.dto.MailRequest;
 import com.EmailSender.dto.MailResponse;
 import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.web.client.RestTemplate;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
@@ -36,25 +32,24 @@ public class EmailService {
 					StandardCharsets.UTF_8.name());
 			// add attachment
 			// helper.addAttachment("logo.png", new ClassPathResource("logo.png"));
-			Template t = config.getTemplate("email-template.ftl");
-			String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
-
+			// Template t = config.getTemplate("template.html");
+			// String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
+			RestTemplate template = new RestTemplate();
+			String url = "http://localhost:8095/getTemplate/" + request.getTo();
+			String result = template.getForObject(url, String.class);
+			System.out.println(result);
 			helper.setTo(request.getTo());
-			helper.setText(html, true);
+			helper.setText(result, true);
 			helper.setSubject(request.getSubject());
 			helper.setFrom(request.getFrom());
 			sender.send(message);
 
 			response.setMessage(" An Email sent to : " + request.getTo() + ". Please verify your email account.");
 			response.setStatus(Boolean.TRUE);
-
-		} catch (MessagingException | IOException | TemplateException e) {
+		} catch (MessagingException e) {
 			response.setMessage("Mail Sending failure : "+e.getMessage());
 			response.setStatus(Boolean.FALSE);
 		}
-
 		return response;
 	}
-	
-
 }
