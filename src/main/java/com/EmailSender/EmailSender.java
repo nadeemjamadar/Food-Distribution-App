e93800d7186648a352d9a30e9be9edb7026f8735
MuzammalHussain6313 <51987198+MuzammalHussain6313@users.noreply.github.com>
2020-05-04 10:20:57 +0500

    template was added to call end point of same application at @Controller and getTamplete endpoint.

--- src/main/java/com/EmailSender/EmailSender.java
+++ src/main/java/com/EmailSender/EmailSender.java
@@ -4,21 +4,24 @@ import com.EmailSender.dto.MailRequest;
 import com.EmailSender.dto.MailResponse;
 import com.EmailSender.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class EmailSender
 {
     @Autowired
     private EmailService service;
 
     @PostMapping("/sendingEmail")
-    public MailResponse sendEmail(@RequestBody MailRequest request) {
+    public MailResponse sendEmail(@RequestBody MailRequest request)
+    {
+//        RestTemplate template = new RestTemplate();
+//        String result = template.getForObject("http://localhost:8095/getJSPage", String.class);
+//        System.out.println(result);
         Map<String, Object> model = new HashMap<>();
         model.put("Name", request.getName());
         model.put("location", "Bangalore,India");
