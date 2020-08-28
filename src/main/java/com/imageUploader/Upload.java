70d4e14e1c3125335ed9bc84fee20fe2a2ee8a03
Muzammil Hussain <51987198+MuzammalHussain6313@users.noreply.github.com>
2020-08-28 07:35:16 +0500

    Update Upload.java

--- src/main/java/com/imageUploader/Upload.java
+++ src/main/java/com/imageUploader/Upload.java
@@ -1,6 +1,7 @@
 package com.imageUploader;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Upload {
 
     @Autowired
