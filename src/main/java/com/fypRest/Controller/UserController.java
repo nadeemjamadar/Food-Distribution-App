ab9a6e6adad08aa6a124656441b5bbfd3e473bab
nadeemjamadar <42204036+nadeemjamadar@users.noreply.github.com>
2021-05-20 12:27:21 +0530

    Update UserController.java

--- src/main/java/com/fypRest/Controller/UserController.java
+++ src/main/java/com/fypRest/Controller/UserController.java
@@ -69,10 +69,10 @@ public class UserController
     public User newUser(@RequestBody User theUser)
     {
         userService.save(theUser);
-        MailRequest request = new MailRequest("Charity App", theUser.getEmail(), "charity.application501@gmail.com", "Confirmation Email");
+        MailRequest request = new MailRequest("Charity App", theUser.getEmail(), "nadeemjamadar39@gmail.com", "Confirmation Email");
         Map<String, Object> model = new HashMap<>();
         model.put("Name", request.getName());
-        model.put("location", "Islamabad, Pakistan");
+        model.put("location", "Bidar, India");
         MailResponse response = service.sendEmail(request, model);
         String responce = response.getMessage();
         System.out.println(responce);
